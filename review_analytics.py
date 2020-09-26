a6b9d1790570cb34222697ed79863893a569592a
林煥培 <abc2464831@gmail.com>
2020-09-26 18:43:42 +0800

    V2

--- review_analytics.py
+++ review_analytics.py
@@ -8,5 +8,12 @@ with open ('reviews.txt', 'r') as f:
 		count += 1
 		if count % 10000 == 0:
 		 	print(len(data))
+print('總共有', len(data),'筆資料')
 
 
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+#len(d) & len(data)要分清楚
+print('留言的平均長度是', sum_len/len(data))
