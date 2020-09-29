ca3947884b769061b400909b34d265bc3ccf65d3
林煥培 <abc2464831@gmail.com>
2020-09-29 18:18:01 +0800

    V3

--- review_analytics.py
+++ review_analytics.py
@@ -13,7 +13,18 @@ print('總共有', len(data),'筆資料')
 
 sum_len = 0
 for d in data:
-	sum_len += len(d)
-
-#len(d) & len(data)要分清楚
+	sum_len = sum_len + len(d)
+#從data裡面 1~1,000,000筆留言的字加起來
+print(sum_len)
+#每一則留言有多少字全部加起來的總和
 print('留言的平均長度是', sum_len/len(data))
+print(len(d))
+#len(d) & len(data)要分清楚
+#len(data):所有留言的字的長度
+#len(d):
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new))
